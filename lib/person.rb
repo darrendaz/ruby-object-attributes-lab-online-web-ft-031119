adfc54b4092ae20a0535244d309f208ae9037df9
Darren L <darren.lou@gmail.com>
2019-03-23 21:34:56 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name
+    @name
+  end
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def breed
+    @breed
+  end
+  
+  def breed=(person_breed)
+    @breed = dog_breed
+  end
+end
